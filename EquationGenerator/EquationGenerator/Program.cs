4a8461230ae81a7a42ffc69db271dc43e7c28226
MaksymShkarupylo <shkarupilo2005@gmail.com>
2022-11-08 21:27:32 +0200

    mmmm

--- EquationGenerator/EquationGenerator/Program.cs
+++ EquationGenerator/EquationGenerator/Program.cs
@@ -115,9 +115,9 @@ namespace EquationGenerator
         public 
         static void Main()
         {
-            //Console.WriteLine(EquationGenerator.GenerateEquationSecondDegree(-10,10));
-            //Console.WriteLine(EquationGenerator.GenerateEquationFirstDegree(-10,10));
-            //Console.WriteLine(EquationGenerator.GenerateEquationSecondDegree(-100,100));
+            Console.WriteLine(EquationGenerator.GenerateEquationSecondDegree(-10,10));
+            Console.WriteLine(EquationGenerator.GenerateEquationFirstDegree(-10,10));
+            Console.WriteLine(EquationGenerator.GenerateEquationSecondDegree(-100,100));
             Console.WriteLine(EquationWriter.GenerateEquationWithKoefs(new []{3,5,8,7,3,4,4,4,4,4,4,4,4,4,4,123124}));
         }
     }
