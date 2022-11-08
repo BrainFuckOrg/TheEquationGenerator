76de3798aeb8cea5a737f12d4c4cc3bad0447c65
MaksymShkarupylo <shkarupilo2005@gmail.com>
2022-11-08 22:22:00 +0200

    1

--- EquationGenerator/EquationGenerator/EquationGenerator.cs
+++ EquationGenerator/EquationGenerator/EquationGenerator.cs
@@ -15,6 +15,36 @@ public static class EquationGenerator
         Int32 coeff2 = solution1 * solution2;
         return EquationWriter.GenerateEquationWithKoefs(new[] { 1, coeff1, coeff2 });
     }
+    
+    public static String GenerateEquationWithSolution(Int16 []solution)
+    {
+        Int32[] coeffs = new Int32[solution.Length+1];
+        coeffs[0] = 1;
+        Int32 n = solution.Length;
+        for (int p = 0; p < n; p++)
+        {
+            Int32 sum = 0;
+            for (Int32 q = 1; q < n-p; q++)
+            {
+                Int32 prod = 1;
+                for (Int32 k = q; k < q+p-1; k++)
+                {
+                    prod *= solution[k - 1];
+                }
+
+                Int32 sum1 = 0;
+                for (Int32 i = q+p; i < n; i++)
+                {
+                    sum1 += solution[i - 1];
+                }
+
+                sum += prod * sum1;
+            }
+
+            coeffs[p + 1] = sum;
+        }
+        return EquationWriter.GenerateEquationWithKoefs(coeffs);
+    }
 
     public static String GenerateEquationFirstDegree(Int16 minSopution, Int16 maxSolution)
     {
--- EquationGenerator/EquationGenerator/Program.cs
+++ EquationGenerator/EquationGenerator/Program.cs
@@ -6,10 +6,10 @@ namespace EquationGenerator
     public static class Program{
         public static void Main()
         {
-            Console.WriteLine(EquationGenerator.GenerateEquationSecondDegree(-10,10));
-            Console.WriteLine(EquationGenerator.GenerateEquationFirstDegree(-10,10));
-            Console.WriteLine(EquationGenerator.GenerateEquationSecondDegree(-100,100));
-            Console.WriteLine(EquationWriter.GenerateEquationWithKoefs(new []{3,5,8,7,3,4,4,4,4,4,4,4,4,4,4,123124}));
+            //Console.WriteLine(EquationGenerator.GenerateEquationSecondDegree(-10,10));
+            //Console.WriteLine(EquationGenerator.GenerateEquationFirstDegree(-10,10));
+            //Console.WriteLine(EquationGenerator.GenerateEquationSecondDegree(-100,100));
+            //Console.WriteLine(EquationWriter.GenerateEquationWithKoefs(new []{3,5,8,7,3,4,4,4,4,4,4,4,4,4,4,123124}));
         }
     }
 }
