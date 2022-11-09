e9a4d9a8f4956490403092d359d314df5bdd181a
MaksymShkarupylo <shkarupilo2005@gmail.com>
2022-11-09 21:05:09 +0200

    delete Generate func

--- EquationGenerator/EquationGenerator/EquationGenerator.cs
+++ EquationGenerator/EquationGenerator/EquationGenerator.cs
@@ -18,34 +18,7 @@ public static class EquationGenerator
     
     public static String GenerateEquationWithSolution(Int16 []solution)
     {
-        Int32[] coeffs = new Int32[solution.Length+1];
-        coeffs[0] = 1;
-        Int32 n = solution.Length;
-        for (int p = 0; p < n; p++)
-        {
-            Int32 sum = 0;
-            for (Int32 q = 1; q <= n-p; q++)
-            {
-                Int32 prod = 1;
-                for (Int32 k = q; k <= q+p-1; k++)
-                {
-                    prod *= solution[k - 1];
-                }
 
-                Int32 sum1 = 0;
-                prod = p == 0 && q > 1 ? 0 : prod;
-                for (Int32 i = q+p; i <= n; i++)
-                {
-                    sum1 += solution[i - 1];
-                }
-                Console.WriteLine(p+") "+prod+" " + sum1);
-
-                sum += prod * sum1;
-            }
-            Console.WriteLine("--- "+p+") "+sum);
-            coeffs[p + 1] = sum * (p % 2 == 1 ? 1 : -1);
-        }
-        return EquationWriter.GenerateEquationWithKoefs(coeffs);
     }
 
     public static String GenerateEquationFirstDegree(Int16 minSopution, Int16 maxSolution)
